cfc7737aa7ecc4657128912318efb4a306919f8c
Andrew Headrick <andrew.headrick@gmail.com>
2012-05-11 15:13:50 -0700

    asdf

--- project/Build.scala
+++ project/Build.scala
@@ -13,10 +13,13 @@ object ApiAnalysis extends Build {
 
   val typesafe = "typesafe repo" at "http://repo.typesafe.com/typesafe/releases"
   
-  val thrift  = "org.apache.thrift" % "libthrift" % "0.7.0"
-  val netty   = "org.jboss.netty" % "netty" % "3.2.7.Final"
-  val akka    = "com.typesafe.akka" % "akka-actor" % "2.0.1"
-  val msgpack = "org.msgpack" % "msgpack" % "0.6.5"
+  val thrift     = "org.apache.thrift" % "libthrift" % "0.7.0"
+  val avro_ipc   = "org.apache.avro" % "avro-ipc" % "1.6.3"
+  val netty      = "org.jboss.netty" % "netty" % "3.2.7.Final"
+  val akka       = "com.typesafe.akka" % "akka-actor" % "2.0.1"
+  val akka_zmq   = "com.typesafe.akka" % "akka-zeromq" % "2.0.1"
+  val msgpack    = "org.msgpack" % "msgpack" % "0.6.0"
+  val scala_test = "org.scalatest" %% "scalatest" % "1.6.1" % "it,test"
   
   
   lazy val all = Project( id = "all", base = file(".") ) aggregate( types )
@@ -31,13 +34,14 @@ object ApiAnalysis extends Build {
 	    ThriftPlugin.thriftSettings     ++
         ProtobufPlugin.protobufSettings ++
  	    SbtAvro.avroSettings            ++
- 	    MsgPackPlugin.msgpackSettings   ++
+ 	    //MsgPackPlugin.msgpackSettings   ++
  	    Seq(
- 	      libraryDependencies ++= List( thrift, netty, akka, msgpack ),
+ 	      libraryDependencies ++= List( thrift, netty, akka, msgpack, akka_zmq, avro_ipc, scala_test ),
  	      resolvers += typesafe
  	    )
  	    
-
-  )
+  ).configs( IntegrationTest )
+   .settings( Defaults.itSettings : _*)
+   .settings( parallelExecution in IntegrationTest := false )
 
 }
---
+++ types/src/main/scala/com/github/bigtoast/apiAnalysis/zmq/Server.scala
@@ -0,0 +1,23 @@
+package com.github.bigtoast.apiAnalysis.zmq
+
+import akka.zeromq._
+import akka.actor._
+
+object Server {
+  
+  val system = ActorSystem()
+  
+  val handler = system.actorOf( Props[Handler] )
+  
+  val router = system.newRouterSocket( Listener( handler ), Connect("tcp://*:1234"), HighWatermark( 10000 ) )
+  val dealer = system.newDealerSocket( Listener( handler ), Connect("tcp://*:1235") )
+  
+  class Handler() extends Actor {
+    
+    def receive :Receive = {
+      case Connecting =>
+      case m :ZMQMessage =>
+      case _ =>
+    }
+  }
+}
